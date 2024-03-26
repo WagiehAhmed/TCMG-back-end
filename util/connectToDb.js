6d88af712720b384beed4d9e3f65548ce3aa8684
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-03-26 04:15:59 +0200

    adding vercel file

--- util/connectToDb.js
+++ util/connectToDb.js
@@ -1,15 +1,24 @@
 const mysql = require("mysql");
-
+require("dotenv").config();
 const pool = mysql.createPool({
-    host:"localhost",
-    user:"tcmtest",
-    password:"amAM123123",
-    database:"tcmtest",
-    connectionLimit:3,
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+
+    // host:"localhost",
+    // user:"root",
+    // password:"",
+    // database:"tcmg",
+    connectionLimit:10,
 })
 
-pool.getConnection(()=>{
-    console.log("connected to tcmg database")
+pool.getConnection((err, conn) => {
+    if(err){
+        console.log(err)
+    }else{
+        // console.log(conn)
+        console.log("Connected successfully")
+    }
 })
-
 module.exports = pool;
