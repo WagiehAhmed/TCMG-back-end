61af619ef5e1ef8077379a08ad896ed571bff89e
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-03-27 14:44:17 +0200

    add origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended:true}));
 app.use(express.static("public"));
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(helmet());
 dotenv.config();
 app.use(express_fileUpload());
