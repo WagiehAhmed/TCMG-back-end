3e88332d0e8bd292b777587156a02ccfedcea70b
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-03-27 17:17:46 +0200

    ase

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -42,21 +42,21 @@ class BlogController {
         return res.status(400).json({ message: "This blog already exists" });
       }
 
-      // var fileName = req.files.image.name;
-      // fileName = Date.now() + "_" + fileName;
-      // const filePath = path.join(
-      //   __dirname,
-      //   "../public/blogs/",
-      //   fileName
-      // );
+      var fileName = req.files.image.name;
+      fileName = Date.now() + "_" + fileName;
+      const filePath = path.join(
+        __dirname,
+        "../public/blogs/",
+        fileName
+      );
 
-      // const image = fileName;
+      const image = fileName;
       const blog = await BlogModal.addBlog(
         title,
         description,
         content,
         category,
-        "image",
+        image,
         date,
         metaKeys
       );
@@ -78,6 +78,8 @@ class BlogController {
       if (blogId) {
         const affectedRows = await BlogModal.deleteBlog(id);
         if (affectedRows) {
+
+          // TODO:  delete from blogs folder
           return res.status(200).json({ blogId });
         } else {
           throw new Error("Invalid id");
